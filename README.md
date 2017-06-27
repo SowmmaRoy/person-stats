4d10338298bee3fe1b0c1185d79745e5de7b1ccd
Sowmma Roy <sroy15@earlham.edu>
2017-06-27 09:13:32 -0400

    Update hw 1

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Update the new_details_ div with the value of the text input.
 
 ### Super Mega Bonus Credit
 
-* Add another inout to the form
+* Add another input to the form
 * Display the value of that input alongside the name in the 'div' (or 'p').
 
 ### Super Mega Bonus Credit Hyper Fighting
--- index.js
+++ index.js
@@ -5,8 +5,20 @@ function updateHeading(ev){
     const heading = document.querySelector('h1')
     heading.textContent = name
     //heading.innerHTML = name
-    
+
+    const height = f.personHeight.value
+    const color = f.personColor.value
+
+    updateStats(name, height, color)
+}
+
+function updateStats(stat, height, color){
+    const statsInfo = document.querySelector('#stats-info')
+    statsInfo.innerHTML = statsInfo.innerHTML + "<br>" + stat + ' ' + height + color
+    //document.querySelector('#stats-info').textContent = stat 
+    statsInfo.style.color = color
 }
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', updateHeading)
+
