95e9e3f7e74a1d5898a9815bafbdc637595f9d7e
Sowmma Roy <sroy15@earlham.edu>
2017-06-27 18:06:49 -0400

    Hw 2 completed

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ Update the new_details_ div with the value of the text input.
 
 ### Super Mega Bonus Credit Hyper Fighting
 * Change the appreance of the paragraph (think CSS) based on a value from the form. _e.g._ Turn the text blue 
+
+Day 2 Home Work
+
+*Create a new function called 'renderColor' that returns a 'div' element.
+*Call that function when adding that 'div' to 'colorItem'.
+
+## Bonus Credit
+*Create a new function called "rednerListItem"
+*Use it to create the list items for each stat.
+
+## Super Mega Bonus Credit
+Create a new function called 'renderList'
+use it to create the list for each person's stats
+call 'renderListItem' from 'renderlist', not directly from 'handleSubmit'.
+
+### Super Mega Bonus Credit Hyper Fighting
+Be strong! Do not resort to the use of 'innerHTML'. Keep using 'document.createElement'
--- index.js
+++ index.js
@@ -1,24 +1,77 @@
-function updateHeading(ev){
+function handleSubmit(ev){
     ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const heading = document.querySelector('h1')
-    heading.textContent = name
-    //heading.innerHTML = name
 
+    const f = ev.target
+       
+    const name = f.personName.value
     const height = f.personHeight.value
     const color = f.personColor.value
 
-    updateStats(name, height, color)
-}
+    const statsContainer = document.querySelector('#stats-container')
 
-function updateStats(stat, height, color){
-    const statsInfo = document.querySelector('#stats-info')
-    statsInfo.innerHTML = statsInfo.innerHTML + "<br>" + stat + ' ' + height + color
-    //document.querySelector('#stats-info').textContent = stat 
-    statsInfo.style.color = color
+    const completeList = renderList(name, height, color)
+
+    statsContainer.appendChild(completeList)
+    // updateStats(name, height, color)
+
 }
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', updateHeading)
+personForm.addEventListener('submit', handleSubmit)
 
+function renderColor(inputColor){
+    const colorBlock = document.createElement('div')
+    colorBlock.style.backgroundColor = inputColor
+    colorBlock.style.width = "5rem"
+    colorBlock.style.height = "8rem"
+    return colorBlock
+}
+
+function renderListItem(inputItem){
+    const itemName = document.createElement('li')
+    itemName.textContent = `${inputItem}`
+    return itemName
+}
+
+function renderList(name,height,color){
+    const fullList = document.createElement('ul')
+    const nameListItem = renderListItem(name)
+    const heightListItem = renderListItem(height)
+    const colorListItem = renderListItem(color)
+    const colorBlock = renderColor(color)
+
+    fullList.appendChild(nameListItem)
+    fullList.appendChild(heightListItem)
+    fullList.appendChild(colorListItem)
+    fullList.appendChild(colorBlock)
+
+    return fullList
+}
+
+
+/*const list = document.createElement('ul')
+const nameItem = document.createElement('li')
+nameItem.textContent = `Name: ${name}`
+list.appendChild(nameItem)
+
+const ageItem = document.createElement('li')
+ageItem.textContent = 'Age: ${age}'
+list.appendChild(ageItem)
+
+const colorItem = document.createElement('li')
+colorItem.textContent= "Favorite Color '
+colorItem.style.backgrwoundColor= favoriteColor
+list.appendChild(colorItem)
+
+const colorDiv = document.createElement('div')
+colorDiv.style.backgroundColor = favoriteColor
+colorDiv.style.width = "6rem"
+colorDiv.style.height = ""
+colorItem. 
+
+const stats = document.querySelector(#'stats')
+stats.appendChild(list)
+
+//Add Statde in an unordered List//
+//Display color in an empty Div//
+*/
